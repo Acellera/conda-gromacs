bb6a13f87c5a400a0905590f0498778fd33d8ae9
M J Harvey <m.j.harvey@acellera.com>
2016-03-12 02:05:44 +0100

    rev

--- package/build.sh
+++ package/build.sh
@@ -9,6 +9,12 @@ tar -zxf gromacs-5.1.2.tar.gz
 mkdir build
 cd build
 mkdir -p "$SP_DIR"
+
+export CC=gcc-4.4
+export CXX=g++-4.4
+export FC=gfortran-4.4
+
+ln -s $(which gcc-4.4) "$HOME/bin/gcc"
 CC=gcc-4.4 CXX=g++-4.4 FC=gfortran-4.4 cmake ../gromacs-5.1.2  -DGMX_BUILD_OWN_FFTW=ON -DGMX_GPU=ON -DGMX_COOL_QUOTES=OFF -DBUILD_SHARED_LIBS=OFF -DGMX_SIMD=SSE4.1 -DCMAKE_INSTALL_PREFIX="$SP_DIR/../../.." -DGMX_PREFER_STATIC_LIBS=ON -DGMX_EXTERNAL_BLAS=OFF  -DGMX_EXTERNAL_LAPACK=OFF
 make -j 2
 make install

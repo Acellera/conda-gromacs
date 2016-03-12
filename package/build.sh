4334ac985b9578c0743d4f6c1d03b753c1565553
M J Harvey <m.j.harvey@acellera.com>
2016-03-12 01:53:14 +0100

    rev

--- package/build.sh
+++ package/build.sh
@@ -9,7 +9,7 @@ tar -zxf gromacs-5.1.2.tar.gz
 mkdir build
 cd build
 mkdir -p "$SP_DIR"
-CC=gcc-4.4 CXX=g++-4.4 FC=gfortran-4.4 cmake ../gromacs-5.1.2  -DGMX_BUILD_OWN_FFTW=ON -DGMX_GPU=ON -DGMX_COOL_QUOTES=OFF -DBUILD_SHARED_LIBS=OFF -DGMX_SIMD=SSE4.1 -DCMAKE_INSTALL_PREFIX="$SP_DIR/../../.." -DGMX_PREFER_STATIC_LIBS=ON -DGMX_EXTERNAL_BLAS=OFF LAPACK_lapack_LIBRARY=/usr/lib/liblapack.a HAVE_LIBM=/usr/lib/x86_64-linux-gnu/libm.a BLAS_atlas_LIBRARY=/usr/lib/libatlas.a BLAS_f77blas_LIBRARY=/usr/lib/libf77blas.a
+CC=gcc-4.4 CXX=g++-4.4 FC=gfortran-4.4 cmake ../gromacs-5.1.2  -DGMX_BUILD_OWN_FFTW=ON -DGMX_GPU=ON -DGMX_COOL_QUOTES=OFF -DBUILD_SHARED_LIBS=OFF -DGMX_SIMD=SSE4.1 -DCMAKE_INSTALL_PREFIX="$SP_DIR/../../.." -DGMX_PREFER_STATIC_LIBS=ON -DGMX_EXTERNAL_BLAS=OFF  -DGMX_EXTERNAL_LAPACK=OFF
 make -j 2
 make install
 rm -rf "$SP_DIR/../../../include"

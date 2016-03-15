f5bc429300bfedb971dcecbc5a79dcd3eac0fde3
MJH <m.j.harvey@acellera.com>
2016-03-15 12:12:36 +0100

    build diddle -- static

--- package/build.sh
+++ package/build.sh
@@ -2,27 +2,55 @@
 printenv
 pwd
 export PATH="$HOME/cmake/bin:$HOME/bin:$PATH"
+VER=5.1.2
 
-wget ftp://ftp.gromacs.org/pub/gromacs/gromacs-5.1.2.tar.gz 
-tar -zxf gromacs-5.1.2.tar.gz
+if [ ! -e "gromacs-${VER}.tar.gz" ]; then
+	wget ftp://ftp.gromacs.org/pub/gromacs/gromacs-${VER}.tar.gz 
+	tar -zxf gromacs-${VER}.tar.gz
+fi
 
 mkdir build
 cd build
-mkdir -p "$SP_DIR"
 
-export CC=gcc-4.4
-export CXX=g++-4.4
-export FC=gfortran-4.4
+which gcc44
+if [ "$?" == "0" ]; then
+	export CC=gcc44
+	export CXX=g++44
+	export FC=gfortran44
+fi
 
-ln -s $(which gcc-4.4) "$HOME/bin/gcc"
-CC=gcc-4.4 CXX=g++-4.4 FC=gfortran-4.4 CFLAGS=--static CXXFLAGS=--static cmake ../gromacs-5.1.2  -DGMX_BUILD_OWN_FFTW=ON -DGMX_GPU=ON -DGMX_COOL_QUOTES=OFF -DBUILD_SHARED_LIBS=OFF -DGMX_SIMD=AVX_256 -DCMAKE_INSTALL_PREFIX="$SP_DIR/../../.." -DGMX_PREFER_STATIC_LIBS=ON -DGMX_EXTERNAL_BLAS=OFF  -DGMX_EXTERNAL_LAPACK=OFF  -DGMX_BUILD_SHARED_EXE=OFF
+which gcc-4.4
+if [ "$?" == "0" ]; then
+	export CC=gcc-4.4
+	export CXX=g++-4.4
+	export FC=gfortran-4.4
+fi
+
+if [ "$SP_DIR" == "" ]; then
+	export PREFIX=/tmp/
+else
+	export PREFIX="$SP_DIR/../../../"
+fi
+mkdir -p "$PREFIX"
+
+export CUDA_HOME="$(dirname $(which nvcc))/../"
+
+ln -s $(which gcc44) "$HOME/bin/gcc"
+CFLAGS=--static CXXFLAGS=--static cmake ../gromacs-5.1.2  -DGMX_BUILD_OWN_FFTW=ON -DGMX_GPU=ON -DGMX_COOL_QUOTES=OFF -DBUILD_SHARED_LIBS=OFF -DGMX_SIMD=AVX_256 -DCMAKE_INSTALL_PREFIX="$PREFIX" -DGMX_PREFER_STATIC_LIBS=ON -DGMX_EXTERNAL_BLAS=OFF  -DGMX_EXTERNAL_LAPACK=OFF  -DGMX_BUILD_SHARED_EXE=OFF -DCUDA_CUDART_LIBRARY="${CUDA_HOME}/lib64/libcudart_static.a"
+
+# Fix CMAKE brain-damage
+echo "Fix CMAKE"
+find . -name link.txt -exec  sed -i 's/-lpthread/& -ldl/g' {} \; -print
+
 make -j 2
+find . -name link.txt -exec  sed -i 's/-lpthread/& -ldl/g' {} \; -print
 make install
-rm -rf "$SP_DIR/../../../include"
-rm -rf "$SP_DIR/../../../lib64"
-rm -rf "$SP_DIR/../../../share/man"
 
-cd "$SP_DIR/../../../bin"
+rm -rf "$PREFIX/include"
+rm -rf "$PREFIX/lib64"
+rm -rf "$PREFIX/share/man"
+
+cd "$PREFIX/bin"
 mv gmx gmx.bin
 echo '#!/bin/sh
 

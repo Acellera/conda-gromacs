dc3ac4468ecb0c586a341133e110d41d9996318d
M J Harvey <m.j.harvey@acellera.com>
2016-03-16 17:50:07 +0100

    diddle

--- package/build.sh
+++ package/build.sh
@@ -27,16 +27,16 @@ mkdir -p "$PREFIX"
 export CUDA_HOME="$(dirname $(which nvcc))/../"
 
 ln -s $(which gcc44) "$HOME/bin/gcc"
-CFLAGS=--static CXXFLAGS=--static cmake ../gromacs-5.1.2  -DGMX_BUILD_OWN_FFTW=ON -DGMX_GPU=ON -DGMX_COOL_QUOTES=OFF -DBUILD_SHARED_LIBS=OFF -DGMX_SIMD=AVX_256 -DCMAKE_INSTALL_PREFIX="$PREFIX" -DGMX_PREFER_STATIC_LIBS=ON -DGMX_EXTERNAL_BLAS=OFF  -DGMX_EXTERNAL_LAPACK=OFF  -DGMX_BUILD_SHARED_EXE=ON -DCUDA_CUDART_LIBRARY="${CUDA_HOME}/lib64/libcudart_static.a"  -DCMAKE_C_FLAGS="-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0"
+cmake ../gromacs-5.1.2  -DGMX_BUILD_OWN_FFTW=ON -DGMX_GPU=ON -DGMX_COOL_QUOTES=OFF -DBUILD_SHARED_LIBS=OFF -DGMX_SIMD=AVX_256 -DCMAKE_INSTALL_PREFIX="$PREFIX" -DGMX_PREFER_STATIC_LIBS=ON -DGMX_EXTERNAL_BLAS=OFF  -DGMX_EXTERNAL_LAPACK=OFF  -DGMX_BUILD_SHARED_EXE=ON -DCUDA_CUDART_LIBRARY="${CUDA_HOME}/lib64/libcudart_static.a"  -DCMAKE_C_FLAGS="-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0"
 
 # Fix CMAKE brain-damage
-echo "Fix CMAKE"
-find /usr -name "librt.a"
-find . -name link.txt -exec  sed -i 's/-lpthread/& -ldl/g' {} \; -print
-find . -name link.txt -exec sed -i 's/-lrt/\/usr\/lib\/x86_64-linux-gnu\/librt.a /g' {} \; -print 
-find . -name link.txt -exec sed -i 's/\/usr\/lib\/x86_64-linux-gnu\/librt.so/ /g' {} \; -print 
-find . -name link.txt -exec  sed -i 's/$/& \/usr\/lib\/x86_64-linux-gnu\/libc.a/g' {} \; -print
-
+#echo "Fix CMAKE"
+#find /usr -name "librt.a"
+#find . -name link.txt -exec  sed -i 's/-lpthread/& -ldl/g' {} \; -print
+#find . -name link.txt -exec sed -i 's/-lrt/\/usr\/lib\/x86_64-linux-gnu\/librt.a /g' {} \; -print 
+#find . -name link.txt -exec sed -i 's/\/usr\/lib\/x86_64-linux-gnu\/librt.so/ /g' {} \; -print 
+#find . -name link.txt -exec  sed -i 's/$/& \/usr\/lib\/x86_64-linux-gnu\/libc.a/g' {} \; -print
+#
 cat ./src/programs/CMakeFiles/gmx.dir/link.txt
 cat ./share/template/CMakeFiles/template.dir/link.txt
 
